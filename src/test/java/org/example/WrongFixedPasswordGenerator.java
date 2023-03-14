b2f527d45530dff60fbe50ec774ff42687ee99d8
slows14tem <106790381+slows14tem@users.noreply.github.com>
2023-03-14 21:09:13 +0900

    feat: 낮은 결합도의 테스트코드 작성

--- src/test/java/org/example/WrongFixedPasswordGenerator.java
+++ src/test/java/org/example/WrongFixedPasswordGenerator.java
@@ -5,6 +5,6 @@ public class WrongFixedPasswordGenerator implements PasswordGenerator {
     @Override
     public String generatePassword() {
 
-        return "ab";  //8글자 생성하는 올바르게 구동되는 passwordGenerator
+        return "ab";  //2글자 생성하는 올바르게 구동되지 않는 passwordGenerator
     }
 }
